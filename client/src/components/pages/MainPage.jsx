197722f0ca78970dacad19542bdba52278633f35
shogadj <kuznetsovsemyondesu@gmail.com>
2024-07-05 14:30:39 +0300

    Mainpage done

--- client/src/components/pages/MainPage.jsx
+++ client/src/components/pages/MainPage.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function MainPage() {
   return (
-    <div>MainPage</div>
-  )
+    <>
+      <div>
+        <h1>Выберите тему:</h1>
+        <Button id="literature" style={{ marginRight: '10px' }} variant="success">
+          Литература
+        </Button>
+        <Button id="history" style={{ marginRight: '10px' }} variant="success">
+          История
+        </Button>
+        <Button id="science" variant="success">
+          Наука
+        </Button>
+      </div>
+    </>
+  );
 }
